b0adfadf211a5919ecf5ffedf7e72451a5360f1e
smhrd <smhrd@DESKTOP-FQCRA9H>
2021-10-20 12:40:11 +0900

    main3

--- BookManagement/src/main.java
+++ BookManagement/src/main.java
@@ -4,14 +4,14 @@ public class main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//		DAO dao = new DAO();
+		BooKDAO dao = new BooKDAO();
 		String b_id;
 		String b_title;
 		String b_autor;
 		String b_publisher;
-		int b_price;
+		int b_price = 0;
 		int cnt=0;
-//		int cnnnt =0;
+		int cnnnt =0;
 		
 		
 		
@@ -32,42 +32,51 @@ public class main {
 				System.out.print("가격 : ");
 				b_price=sc.nextInt();
 				
-//				cnt = dao.insert(b_id,b_title,b_autor,b_publisher,b_price);
+				cnt = dao.insert(b_id,b_title,b_autor,b_publisher,b_price);
 				
-//				if (cnt > 0) {
-//					System.out.println("저장 완료");
-//				} else {
-//					System.out.println("저장 실패");
-//				}
+				if (cnt > 0) {
+					System.out.println("저장 완료");
+				} else {
+					System.out.println("저장 실패");
+				}
 			}else if(menu==2) {
-//				
-//				dao.select();
+					System.out.print("[1]특성도서조회 [2]전체조회 >>");
+					int menu2 = sc.nextInt();
+					if(menu2==1) {
+						System.out.println("조회할 도서아이디 : ");
+						b_id=sc.next();
+						dao.select(b_id);
+					}else if(menu2==2) {
+						dao.selectAll();
+					}else {
+						System.out.println("다시 입력해주세요");
+					}
 				
 			}else if(menu==3) {
 				
 				System.out.print("수정할 도서 아이디: ");
 				b_id = sc.next();
 				
-//				cnt = dao.update(b_id, b_price);
-//				
-//				if (cnt > 0) {
-//					System.out.println("수정완료!");
-//				} else {
-//					System.out.println("수정실패...");
-//				}
+				cnt = dao.update(b_id, b_price);
+				
+				if (cnt > 0) {
+					System.out.println("수정완료!");
+				} else {
+					System.out.println("수정실패...");
+				}
 				
 			}else if(menu==4) {
 				
 				System.out.print("삭제할 도서아이디: ");
 				b_id = sc.next();
 				
-//				cnt = dao.delete(b_id);
+				cnt = dao.delete(b_id);
 				
-//				if (cnt > 0) {
-//					System.out.println("삭제완료!");
-//				} else {
-//					System.out.println("삭제실패...");
-//				}
+				if (cnt > 0) {
+					System.out.println("삭제완료!");
+				} else {
+					System.out.println("삭제실패...");
+				}
 				
 			}else {
 				System.out.println("Java 도서관리 프로그램이 종료됩니다.");
