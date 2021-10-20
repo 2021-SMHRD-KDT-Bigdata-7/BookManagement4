f01889dd7c1379cd54cca682aa86e633019a856e
smhrd <smhrd@DESKTOP-S8D2MR1>
2021-10-20 12:08:14 +0900

    Main 다시올림 이쁜혜인

--- BookManagement/src/main.java
+++ BookManagement/src/main.java
@@ -19,6 +19,7 @@ public class main {
 			System.out.print("[1]도서등록 [2]도서조회 [3]정보수정 [4]삭제 [5] 종료 >>");
 			int menu = sc.nextInt();
 			if(menu ==1) {
+				
 				System.out.print("도서아이디 : ");
 				b_id=sc.next();
 				System.out.print("도서명 : ");
@@ -38,11 +39,34 @@ public class main {
 //					System.out.println("저장 실패");
 //				}
 			}else if(menu==2) {
+//				
 //				dao.select();
 				
 			}else if(menu==3) {
 				
+				System.out.print("수정할 도서 아이디: ");
+				b_id = sc.next();
+				
+//				cnt = dao.update(b_id, b_price);
+//				
+//				if (cnt > 0) {
+//					System.out.println("수정완료!");
+//				} else {
+//					System.out.println("수정실패...");
+//				}
+				
 			}else if(menu==4) {
+				
+				System.out.print("삭제할 도서아이디: ");
+				b_id = sc.next();
+				
+//				cnt = dao.delete(b_id);
+				
+//				if (cnt > 0) {
+//					System.out.println("삭제완료!");
+//				} else {
+//					System.out.println("삭제실패...");
+//				}
 				
 			}else {
 				System.out.println("Java 도서관리 프로그램이 종료됩니다.");
