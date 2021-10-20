ed7bac1ca7e59b9948a972c7f594e95738495de1
smhrd <smhrd@DESKTOP-FQCRA9H>
2021-10-20 11:37:38 +0900

    main

---
+++ BookManagement/src/main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		DAO dao = new DAO();
+		
+		while (true) {
+			System.out.println("[Java 도서관리 프로그램]");
+			System.out.print("[1]도서등록 [2]도서조회 [3]정보수정 [4]삭제 [5] 종료 >>");
+			int menu = sc.nextInt();
+			if(menu ==1) {
+				
+			}else if(menu==2) {
+				
+			}else if(menu==3) {
+				
+			}else if(menu==4) {
+				
+			}else {
+				System.out.println("Java 도서관리 프로그램이 종료됩니다.");
+				break;
+			}
+		}
+
+	}
+
+}
